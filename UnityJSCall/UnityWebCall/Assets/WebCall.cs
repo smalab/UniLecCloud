7604b7dbc6cd6dc82e8f46b5027142652e0bf951
yanoty3d <vzb10326@me.com>
2014-07-08 09:03:07 +0900

    UnityとWeb pageのjavascriptをやり取りする為のスクリプト。Unityのボタンを押す事でビデオの再生位置が変わり、jsからの命令でUnityの関数が実行され、その関数がconsole.logをブラウザに返す。

---
+++ UnityJSCall/UnityWebCall/Assets/WebCall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebCall : MonoBehaviour {
+
+	// Use this for initialization
+	void OnGUI() {
+ 		if (GUI.Button(new Rect(10, 70, 50, 30), "Click")){
+			Application.ExternalCall( "UnityTest  ",10);  
+ 		}		
+	}
+
+	void MyFunction(string param)
+	{
+		Application.ExternalCall("console.log", param); 
+	}
+}
